3ffae186f0f13fb3f6cf29aafa30b9af02dcd38c
kidpaul94 <kidpaul94@gmail.com>
2022-08-29 00:51:17 -0400

    Comments update

--- engine.py
+++ engine.py
@@ -18,7 +18,7 @@ def validation(model, criterion, bag_size, test_path, transform):
 def engine(model, device, criterion, optimizer, lr_scheduler, scaler, total_data, iterate, batch_size, bag_size, train_path, test_path):
     '''
     This function is written for training the binary classification MIL model
-    Several modification is needed for multiclass classification 
+    Several modifications are needed for multiclass classification 
     (e.g., line 33 ~ 34 & line 50 ~ 58)
     '''
     roots = [f'{train_path}/{f}' for f in listdir(train_path)]
